56e443bfe055714cce7293c0fc508b9ad128c512
benzhe <ben304zhe@gmail.com>
2021-08-07 18:26:25 +0800

    fix: build passthrough in mainland china & add tproxy module

--- packages/audio/alsa-topology-conf/package.mk
+++ packages/audio/alsa-topology-conf/package.mk
@@ -6,7 +6,7 @@ PKG_VERSION="1.2.4"
 PKG_SHA256="55e0e6e42eca4cc7656c257af2440cdc65b83689dca49fc60ca0194db079ed07"
 PKG_LICENSE="BSD-3c"
 PKG_SITE="http://www.alsa-project.org/"
-PKG_URL="ftp://ftp.alsa-project.org/pub/lib/alsa-topology-conf-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://src.fedoraproject.org/repo/pkgs/alsa-lib/alsa-topology-conf-1.2.4.tar.bz2/sha512/e5b367a23f42ed2c2a83f3dd9df264b4e054f5ba7c4be98520418598f0b06a11627498a8a6ef943522b209951645f83bbbbfb32db7c9c8260aa5db08358970cb/alsa-topology-conf-1.2.4.tar.bz2"
 PKG_LONGDESC="ALSA topology configuration files"
 PKG_TOOLCHAIN="manual"
 
--- packages/audio/alsa-ucm-conf/package.mk
+++ packages/audio/alsa-ucm-conf/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2020-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="alsa-ucm-conf"
-PKG_VERSION="1.2.4"
+PKG_VERSION="1.2.2"
 PKG_SHA256="2c3b535c77dcb9aaf62a61f4f8324f1ab184162f105f7ec9ed1e37c742fcd340"
 PKG_LICENSE="BSD-3c"
 PKG_SITE="http://www.alsa-project.org/"
-PKG_URL="ftp://ftp.alsa-project.org/pub/lib/alsa-ucm-conf-${PKG_VERSION}.tar.bz2"
+PKG_URL="https://src.fedoraproject.org/repo/pkgs/alsa-lib/alsa-ucm-conf-1.2.4.tar.bz2/sha512/9043460e92b2ed44757b08b9faca888e8bfae40d84e4ad7e7df44df2bb3b0617e86ef23783973accd62fb6681788262e67212e2bf67178d75781e57a0fa346d2/alsa-ucm-conf-1.2.4.tar.bz2"
 PKG_LONGDESC="ALSA Use Case Manager configuration (and topologies)"
 PKG_TOOLCHAIN="manual"
 
